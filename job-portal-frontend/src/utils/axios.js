9c8735cfd9f23072f586ab8e20a63544d1b2bdfc
Muhammad Faizan Hassan <149832717+muhammadfaizanhassan@users.noreply.github.com>
2025-05-04 17:47:07 +0500

    Update axios.js

--- job-portal-frontend/src/utils/axios.js
+++ job-portal-frontend/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://jobportal-mern-9wgs.onrender.com/api',
 });
 
 // Attach JWT token to every request if present
